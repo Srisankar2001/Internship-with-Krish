d9556134f5f58b7154de871436d5e87a4ab8d763
Srisankar <srisank-se20028@stu.kln.ac.lk>
2025-10-20 16:52:16 +0530

    finished

--- Trip_Planner_360/client/client.js
+++ Trip_Planner_360/client/client.js
@@ -7,19 +7,17 @@ const url_v2 =
   "http://localhost:3000/v2/trip/search?from=srilanka&to=india&date=10-10-2025";
 
 const version = process.env.API_VERSION;
-console.log(version);
-console.log(typeof version);
 if (version == "1") {
   try {
     const response = await fetch(url_v1, { method: "GET" });
-    console.log(response);
+    console.log(JSON.stringify(await response.json(), null, 2));
   } catch (error) {
     console.log("Error");
   }
 } else if (version == "2") {
   try {
     const response = await fetch(url_v2, { method: "GET" });
-    console.log(response);
+    console.log(JSON.stringify(await response.json(), null, 2));
   } catch (error) {
     console.log("Error");
   }
